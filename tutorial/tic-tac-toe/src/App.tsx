39a6d72c1f8221650f803ec933c9937b8d294f6d
youngbeom-mrt <youngbeom.rhee@myrealtrip.com>
2025-03-16 13:23:34 +0900

    feat: state를 상위 컴포넌트로 끌어올리기

--- tutorial/tic-tac-toe/src/App.tsx
+++ tutorial/tic-tac-toe/src/App.tsx
@@ -1,35 +1,45 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState<string | null>(null);
-
-  function handleClick() {
-    setValue('X');
-  }
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
+
+  function handleClick(i: number) {
+    // 원본의 훼손 방지
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
