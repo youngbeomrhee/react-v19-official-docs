88bdf204d61543ec563a4df0130451bcede3b548
youngbeom-mrt <youngbeom.rhee@myrealtrip.com>
2025-03-16 13:38:04 +0900

    feat: winner 정하는 로직 추가

--- tutorial/tic-tac-toe/src/App.tsx
+++ tutorial/tic-tac-toe/src/App.tsx
@@ -15,17 +15,32 @@ function Square({
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   function handleClick(i: number) {
+    // 이미 값이 있으면 변경 못하도록 return
+    if (squares[i] !== null || calculateWinner(squares)) {
+      return;
+    }
     // 원본의 훼손 방지
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -43,4 +58,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
