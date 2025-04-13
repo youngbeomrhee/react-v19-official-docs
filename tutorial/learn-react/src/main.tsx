1dc65d08f7cd396d1f547ac6621fd46ea620325f
youngbeom-mrt <youngbeom.rhee@myrealtrip.com>
2025-04-13 15:46:16 +0900

    feat: 조건부 렌더링

--- tutorial/learn-react/src/main.tsx
+++ tutorial/learn-react/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import PackingList from "./PackingList";
+// import App from "./App";
 // import Gallery from "./Gallery";
 // import TodoList from "./TodoList";
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <Gallery /> */}
     {/* <TodoList /> */}
-    <App />
+    {/* <App /> */}
+    <PackingList />
   </StrictMode>
 );
